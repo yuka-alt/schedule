8d5b3a639f37033a91f443548ef60a64f9c3d667
amaike <amaikepro0201@gmail.com>
2021-11-07 11:14:32 +0900

    html追加

--- laravel/resources/views/schedule/index.blade.php
+++ laravel/resources/views/schedule/index.blade.php
@@ -1,8 +1,48 @@
-<!-- タスク一覧表示 -->
+<!--head-->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="スケジュール帳">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="login.css">
+  <title>スケジュール帳</title>
+</head>
+
+<!--ログイン画面-->
+<body>
+  <h1>スケジュール</h1>
+  <div class="login">
+    <table>
+      <form action="login.html" method="get"></form>
+        <tr>
+          <th>ID</th>
+            <td>
+              <input type="text" name="id" id="" size="24">
+            </td>
+        </tr>
+        <tr>
+          <th>パスワード</th>
+            <td>
+              <input type="password" name="password" id="" size="24">
+            </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <input type="submit" value="ログイン">
+          </td>
+        </tr>
+    </table>
+  </div>
+</body>
+</html>
+
+<!-- スケジュール一覧表示 -->
 @if (count($schedules) > 0)
 <div class="panel panel-default">
     <div class="panel-heading">
-        テスト１
+        <!--test-->
     </div>
  
     <div class="panel-body">
@@ -10,7 +50,7 @@
  
             <!-- テーブルヘッダ -->
             <thead>
-                <th>テスト2</th>
+                <th> <!--test--></th>
                 <th>&nbsp;</th>
             </thead>
  
