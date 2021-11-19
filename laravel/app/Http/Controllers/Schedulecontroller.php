46253ef303bc8ed31857f899469b4a09ae74a7e9
saki4603 <no_ag7221@icloud.com>
2021-11-20 00:23:11 +0900

    変更

--- laravel/app/Http/Controllers/Schedulecontroller.php
+++ laravel/app/Http/Controllers/Schedulecontroller.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\schedule;
+
 class Schedulecontroller extends Controller
 {
-    //
+    public function month(){
+        return view('schedule.1month');
+    }
 }
