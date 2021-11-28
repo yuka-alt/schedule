6534ccd7ed2159c53cf35a0393cb451d42fb845c
Yuka <yuka@YukanoMacBook-ea.local>
2021-11-28 21:39:48 +0900

    確認用

--- laravel/resources/views/register.blade.php
+++ laravel/resources/views/register.blade.php
@@ -6,10 +6,24 @@ ini_set('display_errors',1);
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>スケジュール登録</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> -->
+
+    <!-- Styles -->
+    <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+    
 </head>
 <body>
     <h1>スケジュール登録</h1>
