0c1d24e15fbf703ffeef7f9b59632b7fd483f5c5
天池 <amaike0201@gmail.com>
2022-01-09 11:18:16 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        if (\App::enviroment(['production'])){
+        if (\App::environment(['production'])){
             \URL::forceScheme('https');
         }
     }
