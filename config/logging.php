4b321cc818077f99c1c61e5518593ecb78a67aa4
天池 <amaike0201@gmail.com>
2022-01-09 12:12:20 +0900

    errolog

--- config/logging.php
+++ config/logging.php
@@ -55,8 +55,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            //'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
